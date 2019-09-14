606d19bdd094f10b1e6881945d3d33bb43ddd8a2
Afsar Sheikh <sheikhafsar72@gmail.com>
2019-09-14 20:35:09 +0530

    spring boot -Hierachical Properties

--- src/main/java/sheikhafsar/in/springboot/controller/RestEndpoints.java
+++ src/main/java/sheikhafsar/in/springboot/controller/RestEndpoints.java
@@ -1,8 +1,39 @@
 package sheikhafsar.in.springboot.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class RestEndpoints {
+
+    @Value("${default.course.name}")
+    private String cName;
+    @Value("${default.course.chapterCount}")
+    private int chapCount;
+
+    @Autowired
+    private CourseConfiguration courseConfiguration;
+
+    @RequestMapping("/defaultCourse") //to create endpoint
+    public Course getdefaultCourse(@RequestParam(value="name",defaultValue="Spring Boot",required = false)String name,
+                              @RequestParam(value="chapterCount",defaultValue="2",required = false)int chapterCount){
+        return new Course(cName,chapCount);
+    }
+
+    @RequestMapping("/gethierarchical") //to create endpoint
+    public HashMap<String,Object> getConfigAnnotateProperties(){
+        HashMap<String,Object> map = new HashMap<String,Object>();
+
+        map.put("name",courseConfiguration.getName());
+        map.put("chapterCount",courseConfiguration.getChapterCount());
+        map.put("rating",courseConfiguration.getRating());
+        map.put("author",courseConfiguration.getAuthor());
+
+        return map;
+    }
+
     @RequestMapping("/course") //to create endpoint
     public Course getEndpoint(@RequestParam(value="name",defaultValue="Spring Boot",required = false)String name,
                               @RequestParam(value="chapterCount",defaultValue="2",required = false)int chapterCount){
